4836b5e5c710ca656c5db1ee1a3baf212b65d4e4
Marcos Garcia <marcos.garcia5667@gmail.com>
2024-01-12 19:01:44 -0800

    Final world editor

--- js/math/graph.js
+++ js/math/graph.js
@@ -1,87 +1,88 @@
 class Graph {
-    constructor(points = [], segments = []){
-        this.points = points;
-        this.segments = segments;
-    }
+   constructor(points = [], segments = []) {
+      this.points = points;
+      this.segments = segments;
+   }
 
+   static load(info) {
+      const points = info.points.map((i) => new Point(i.x, i.y));
+      const segments = info.segments.map((i) => new Segment(
+         points.find((p) => p.equals(i.p1)),
+         points.find((p) => p.equals(i.p2))
+      ));
+      return new Graph(points, segments);
+   }
 
-    static load(info) {
-        const points = info.points.map((i) => new Point(i.x, i.y));
-        const segments = info.segments.map((i) => new Segment(
-            points.find((p) => p.equals(i.p1)),
-            points.find((p) => p.equals(i.p2))
-        ));
-        return new Graph(points, segments);
-    }
+   hash() {
+      return JSON.stringify(this);
+   }
 
+   addPoint(point) {
+      this.points.push(point);
+   }
 
-    addPoint(point){
-        this.points.push(point);
-    }
+   containsPoint(point) {
+      return this.points.find((p) => p.equals(point));
+   }
 
-    containsPoint(point){
-        // (p) is each point in this.points
-        return this.points.find((p) => p.equals(point));
-    }
+   tryAddPoint(point) {
+      if (!this.containsPoint(point)) {
+         this.addPoint(point);
+         return true;
+      }
+      return false;
+   }
 
-    tryAddPoint(point){
-        if (!this.containsPoint(point)){
-            this.addPoint(point);
-            return true;
-        }
+   removePoint(point) {
+      const segs = this.getSegmentsWithPoint(point);
+      for (const seg of segs) {
+         this.removeSegment(seg);
+      }
+      this.points.splice(this.points.indexOf(point), 1);
+   }
 
-        return false;
-    }
+   addSegment(seg) {
+      this.segments.push(seg);
+   }
 
-    removePoint(point){
-        const segs = this.getSegmentsWithPoint(point);
-        for (const seg of segs){
-            this.removeSegment(seg);
-        }
-        this.points.splice(this.points.indexOf(point), 1);
-    }
+   containsSegment(seg) {
+      return this.segments.find((s) => s.equals(seg));
+   }
 
-    addSegment(seg){
-        //repeats segments must fix
-        this.segments.push(seg);
-    }
+   tryAddSegment(seg) {
+      if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+         this.addSegment(seg);
+         return true;
+      }
+      return false;
+   }
 
-    containsSegment(seg){
-        return this.segments.find((s) => s.equals(seg));
-    }
+   removeSegment(seg) {
+      this.segments.splice(this.segments.indexOf(seg), 1);
+   }
 
-    tryAddSegment(seg){
-        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)){
-            this.addSegment(seg);
-            return true;
-        }
+   getSegmentsWithPoint(point) {
+      const segs = [];
+      for (const seg of this.segments) {
+         if (seg.includes(point)) {
+            segs.push(seg);
+         }
+      }
+      return segs;
+   }
 
-        return false;
-    }
+   dispose() {
+      this.points.length = 0;
+      this.segments.length = 0;
+   }
 
+   draw(ctx) {
+      for (const seg of this.segments) {
+         seg.draw(ctx);
+      }
 
-    removeSegment(seg){
-        this.segments.splice(this.segments.indexOf(seg), 1);
-    }
-
-    getSegmentsWithPoint(point){
-        return this.segments.filter((seg) => seg.includes(point));
-    }
-
-
-    dispose(){
-        this.points.length = 0;
-        this.segments.length = 0;
-    }
-
-
-    draw(ctx){
-        for (const seg of this.segments){
-            seg.draw(ctx);
-        }
-
-        for (const point of this.points){
-            point.draw(ctx);
-        }
-    }
+      for (const point of this.points) {
+         point.draw(ctx);
+      }
+   }
 }
--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -1,26 +1,40 @@
 class Envelope {
-    constructor(skeleton, width) {
-        this.skeleton = skeleton;
-        this.poly = this.#generatePolygon(width);
-    }
+   constructor(skeleton, width, roundness = 1) {
+      if (skeleton) {
+         this.skeleton = skeleton;
+         this.poly = this.#generatePolygon(width, roundness);
+      }
+   }
 
-    #generatePolygon(width) {
-        const { p1, p2 } = this.skeleton;
+   static load(info) {
+      const env = new Envelope();
+      env.skeleton = new Segment(info.skeleton.p1, info.skeleton.p2);
+      env.poly = Polygon.load(info.poly);
+      return env;
+   }
 
-        const radius = width / 2;
+   #generatePolygon(width, roundness) {
+      const { p1, p2 } = this.skeleton;
 
-        const alpha = angle(subtract(p2, p1));
-        const alpha_cw = alpha + Math.PI / 2;
-        const alpha_ccw = alpha - Math.PI / 2;
-        const p1_ccw = translate(p1, alpha_ccw, radius);
-        const p2_ccw = translate(p2, alpha_ccw, radius);
-        const p2_cw = translate(p2, alpha_cw, radius);
-        const p1_cw = translate(p1, alpha_cw, radius);
+      const radius = width / 2;
+      const alpha = angle(subtract(p1, p2));
+      const alpha_cw = alpha + Math.PI / 2;
+      const alpha_ccw = alpha - Math.PI / 2;
+      
+      const points = [];
+      const step = Math.PI / Math.max(1, roundness);
+      const eps = step / 2;
+      for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
+         points.push(translate(p1, i, radius));
+      }
+      for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
+         points.push(translate(p2, Math.PI + i, radius));
+      }
 
-        return new Polygon([p1_ccw, p2_ccw, p2_cw, p1_cw]);
-    }
+      return new Polygon(points);
+   }
 
-    draw(ctx) {
-        this.poly.draw(ctx);
-    }
+   draw(ctx, options) {
+      this.poly.draw(ctx, options);
+   }
 }
